8c48ec8b51a6e001455034d68da2939a86974ee9
ahmad5521 <a.wv@live.com>
2018-03-23 09:45:59 +0300

    Add project files.

---
+++ MyFirstPWA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstPWA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
